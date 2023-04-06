2f190b86ef599424cd9d2f208ef6388c52165921
wahidfebr <wahidfebr@gmail.com>
2023-04-06 07:53:27 +0700

    feat: add password checker page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,22 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
+<script>
+import { RouterView } from 'vue-router'
+
+import Sidebar from './components/Sidebar.vue'
+
+export default {
+  components: {
+    Sidebar
+  },
+}
 </script>
 
 <template>
-  <RouterView />
+  <div class="flex">
+    <div class="w-1/5 h-screen fixed">
+      <Sidebar />
+    </div>
+    <div class="w-4/5 ml-auto mr-24">
+      <RouterView />
+    </div>
+  </div>
 </template>
